2c564128d35f26057317f0a30bf58f2d7bc314f4
Anthony Bouch <anthony@infonomic.io>
2026-01-12 09:02:11 +0700

    feat: correct props for Layout

--- apps/next/src/app/(site)/layout.tsx
+++ apps/next/src/app/(site)/layout.tsx
@@ -2,11 +2,7 @@ import type React from 'react'
 
 import '@/ui/styles/global.css'
 
-type RootLayoutProps = Readonly<{
-  children: React.ReactNode
-}>
-
-export default function RootLayout({ children }: RootLayoutProps): React.JSX.Element {
+export default function RootLayout({ children }: LayoutProps<'/'>): React.JSX.Element {
   return (
     <html className="dark scroll-smooth" lang="en" data-theme="dark">
       <body>{children}</body>
