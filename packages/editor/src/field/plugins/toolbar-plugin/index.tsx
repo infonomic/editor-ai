387dde652151d6edc7e25494df7718a3464b6e82
Anthony Bouch <anthony@infonomic.io>
2026-05-27 07:02:01 +0700

    style: lint fixes

--- packages/editor/src/field/plugins/toolbar-plugin/index.tsx
+++ packages/editor/src/field/plugins/toolbar-plugin/index.tsx
@@ -376,9 +376,9 @@ export function ToolbarPlugin(): React.JSX.Element {
         anchorNode.getKey() === 'root'
           ? anchorNode
           : $findMatchingParent(anchorNode, (e) => {
-            const parent = e.getParent()
-            return parent !== null && $isRootOrShadowRoot(parent)
-          })
+              const parent = e.getParent()
+              return parent !== null && $isRootOrShadowRoot(parent)
+            })
 
       if (element === null) {
         element = anchorNode.getTopLevelElementOrThrow()
@@ -926,7 +926,6 @@ export function ToolbarPlugin(): React.JSX.Element {
         .map((item) => (
           <Fragment key={item.id}>{item.node}</Fragment>
         ))}
-
     </div>
   )
 }
